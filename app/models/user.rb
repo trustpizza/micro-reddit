538317f8463c1effa35010e5ce118a9a921522ef
trustpizza <maxelolsson@gmail.com>
2022-11-30 08:42:33 -0600

    Create User class in Application record, saved user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   validates :username, presence: true, length: { in: 4..12 }, uniqueness: true
   validates :email, uniqueness: true, presence: true
   validates :password, length: { in: 6..16 }, presence: true
+
+
 end
