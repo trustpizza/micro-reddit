7818cd9fee53bd5988d6dff78aa4d2966ebf5318
trustpizza <maxelolsson@gmail.com>
2022-11-30 13:45:08 -0600

    Remove Author_id column

--- db/migrate/20221130185735_create_comments.rb
+++ db/migrate/20221130185735_create_comments.rb
@@ -1,8 +1,9 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.string :title
       t.text :body
+      t.references :post 
+      t.references :user
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_30_164431) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_30_194442) do
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.integer "post_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "posts", id: false, force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.integer "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id", null: false
